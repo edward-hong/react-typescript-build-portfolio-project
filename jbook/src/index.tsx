7f11a088be1b772fb52e95b9e569ee269a6d8ef4
Edward Hong <edward.hong527@gmail.com>
2021-05-09 21:53:59 +1200

    S16L187 React to Redux Connection

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,14 +1,19 @@
-import 'bulmaswatch/superhero/bulmaswatch.min.css'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+
+import 'bulmaswatch/superhero/bulmaswatch.min.css'
 
 // import CodeCell from './components/code-cell'
 import TextEditor from './components/text-editor'
+import { store } from './state'
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+      </div>
+    </Provider>
   )
 }
 
