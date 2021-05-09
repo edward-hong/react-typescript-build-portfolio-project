90789dc3b7a565e430304db89b7581297c546ad6
Edward Hong <edward.hong527@gmail.com>
2021-05-09 23:19:26 +1200

    S17L195 Swapping Cell Placement

--- jbook/src/state/reducers/cellsReducer.ts
+++ jbook/src/state/reducers/cellsReducer.ts
@@ -31,7 +31,18 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
       state.order = state.order.filter(id => id !== action.payload)
       break
     case ActionType.MOVE_CELL:
-      return state
+      const { direction } = action.payload
+      const index = state.order.findIndex(id => id === action.payload.id)
+      const targetIndex = direction === 'up' ? index - 1 : index + 1
+
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+        break
+      }
+
+      state.order[index] = state.order[targetIndex]
+      state.order[targetIndex] = action.payload.id
+
+      break
     case ActionType.INSERT_CELL_BEFORE:
       return state
     default:
