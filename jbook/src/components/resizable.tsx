e49e1aeb520953997a8f5535f9d1626daefb4437
Edward Hong <edward.hong527@gmail.com>
2021-05-09 12:28:24 +1200

    S13L154 On Resize Events

--- jbook/src/components/resizable.tsx
+++ jbook/src/components/resizable.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { ResizableBox, ResizableBoxProps } from 'react-resizable'
 
 import './resizable.css'
@@ -8,6 +9,17 @@ interface ResizableProps {
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   let resizableProps: ResizableBoxProps
+
+  useEffect(() => {
+    const listener = () => {
+      console.log(window.innerWidth, window.innerHeight)
+    }
+    window.addEventListener('resize', listener)
+
+    return () => {
+      window.removeEventListener('resize', listener)
+    }
+  }, [])
 
   if (direction === 'horizontal') {
     resizableProps = {
