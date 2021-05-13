c0cf890eed75a11db1c0f03e86f08680add531ec
Edward Hong <edward.hong527@gmail.com>
2021-05-14 00:41:23 +1200

    S18L209 Applying Custom CSS

--- jbook/src/components/cell-list-item.tsx
+++ jbook/src/components/cell-list-item.tsx
@@ -4,6 +4,8 @@ import ActionBar from './action-bar'
 
 import { Cell } from '../state'
 
+import './cell-list-item.css'
+
 interface CellListItemProps {
   cell: Cell
 }
@@ -18,9 +20,9 @@ const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   }
 
   return (
-    <div>
-      <ActionBar id={cell.id} />
+    <div className="cell-list-item">
       {child}
+      <ActionBar id={cell.id} />
     </div>
   )
 }
