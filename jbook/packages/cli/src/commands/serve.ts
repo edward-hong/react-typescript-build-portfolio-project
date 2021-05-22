9514e22a4e353783d8771a2106baec5a82e60ce9
edward-hong <edward.hong527@gmail.com>
2021-05-22 22:15:17 +1200

    S25L309 Refactoring Package Names

--- jbook/packages/cli/src/commands/serve.ts
+++ jbook/packages/cli/src/commands/serve.ts
@@ -1,6 +1,6 @@
 import path from 'path'
 import { Command } from 'commander'
-import { serve } from 'local-api'
+import { serve } from '@jsnote-eh/local-api'
 
 const isProduction = process.env.NODE_ENV === 'production'
 
